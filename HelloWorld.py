d93f078f6d5d06872cc2efdaeeae8064996857bf
Julie Plante <71974620+plantejulie42@users.noreply.github.com>
2025-01-11 21:05:52 -0500

    add comment to all in print statement

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,3 @@
 # Code written by Julie Plante
-print("Hello, World!")  # one simple python statement.
+print("Hello, World! Hope you're all good!")  # one simple python statement.
 print("This is a short Python code runned in Python3.") # added for fun.
